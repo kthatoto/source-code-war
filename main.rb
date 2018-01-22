dc3538b6f4db66ef916c3f198defa4e8028e7c93
kthatoto <kthatoto@gmail.com>
2018-01-23 01:31:49 +0900

    Added error handling about argv

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
 require 'curses'
-require 'pp'
 
-file = File.read('pattern.rb')
+file_name = ARGV[0]
+unless file_name
+  puts "file name is required"
+  exit
+end
+unless File.exist?(file_name)
+  puts "#{file_name} not exists"
+  exit
+end
+file = File.read(file_name)
 lines = file.each_line.to_a
 
 lines.each {|line| puts line}
