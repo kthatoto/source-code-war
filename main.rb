a7392d0aa6c64ee1ce912887204363cb4192f7b0
kthatoto <kthatoto@gmail.com>
2018-02-23 00:13:09 +0900

    Implement move by hjkl

--- main.rb
+++ main.rb
@@ -1,8 +1,5 @@
 require 'bundler/setup'
 require 'curses'
-require 'syntax/convertors/html'
-require 'syntax'
-require 'colorize'
 
 file_name = ARGV[0]
 unless file_name
@@ -13,21 +10,49 @@ unless File.exist?(file_name)
   puts "#{file_name} not exists"
   exit
 end
-# String.color_samples
-tokenizer = Syntax.load('ruby')
-tokenizer.tokenize( File.read( file_name ) ) do |token|
-  # puts "group(#{token.group}, #{token.instruction}) lexeme(#{token})"
-  case token.group
-  when :ident, :normal
-    print token.to_s
-  when :punct
-    print token.to_s.light_green
-  when :symbol
-    print token.to_s.magenta
+
+file = File.read(file_name)
+lines = file.each_line.to_a
+
+# lines.each {|line| puts line}
+
+Curses.init_screen
+offset = (Curses.lines - lines.count) / 2
+if offset < 0
+  puts "file lines is larger than screen hight"
+  exit
+end
+Curses.setpos(offset, 0)
+lines.each{|line| Curses.addstr(line)}
+
+Curses.noecho
+while true
+  key = Curses.getch
+  case key
+  when 'q'
+    break
+  when 'h'
+    Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx - 1)
+  when 'j'
+    Curses.setpos(Curses.stdscr.cury + 1, Curses.stdscr.curx)
+  when 'k'
+    Curses.setpos(Curses.stdscr.cury - 1, Curses.stdscr.curx)
+  when 'l'
+    Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx + 1)
+  end
+
+  if Curses.inch != 32
+  case key
+    when 'h'
+      Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx + 1)
+    when 'j'
+      Curses.setpos(Curses.stdscr.cury - 1, Curses.stdscr.curx)
+    when 'k'
+      Curses.setpos(Curses.stdscr.cury + 1, Curses.stdscr.curx)
+    when 'l'
+      Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx - 1)
+    end
   end
 end
 
-# file = File.read(file_name)
-# lines = file.each_line.to_a
-#
-# lines.each {|line| puts line}
+Curses.close_screen
