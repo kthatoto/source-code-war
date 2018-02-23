215161d34801482a018043a947817831b1a1a7de
kthatoto <kthatoto@gmail.com>
2018-02-24 01:01:08 +0900

    Refactored and added color

--- main.rb
+++ main.rb
@@ -2,56 +2,64 @@ require 'bundler/setup'
 require 'curses'
 
 file_name = ARGV[0]
-unless file_name
-  puts "file name is required"
-  exit
-end
-unless File.exist?(file_name)
-  puts "#{file_name} not exists"
-  exit
-end
+
+(puts "file name is required"; exit) unless file_name
+(puts "#{file_name} not exists"; exit) unless File.exist?(file_name)
 
 file = File.read(file_name)
 lines = file.each_line.to_a
 
-# lines.each {|line| puts line}
-
 Curses.init_screen
+Curses.start_color
+# Curses.use_default_colors
+
 offset = (Curses.lines - lines.count) / 2
-if offset < 0
-  puts "file lines is larger than screen hight"
-  exit
-end
+(puts "file lines is larger than screen hight"; exit) if offset < 0
+
+###playground###==========###
+# $col_a = [
+#   Curses::COLOR_BLACK, Curses::COLOR_BLUE, Curses::COLOR_CYAN, Curses::COLOR_GREEN,
+#   Curses::COLOR_MAGENTA, Curses::COLOR_RED, Curses::COLOR_WHITE, Curses::COLOR_YELLOW
+# ]
+
+###=======================###
+
+#=init colors=#
+Curses.init_pair(1, Curses::COLOR_CYAN, Curses::COLOR_BLACK)
+#=============#
+
 Curses.setpos(offset, 0)
-lines.each{|line| Curses.addstr(line)}
+lines.each{|line|
+  line.chars.each{|char|
+    if char.match(/[0-9A-Z]/)
+      Curses.attron(Curses.color_pair(1))
+      Curses.addch(char)
+      Curses.attroff(Curses.color_pair(1))
+    else
+      Curses.addch(char)
+    end
+  }
+}
 
 Curses.noecho
 while true
   key = Curses.getch
+  current_position = {y: Curses.stdscr.cury, x: Curses.stdscr.curx}
   case key
   when 'q'
     break
   when 'h'
-    Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx - 1)
+    Curses.setpos(current_position[:y], current_position[:x] - 1)
   when 'j'
-    Curses.setpos(Curses.stdscr.cury + 1, Curses.stdscr.curx)
+    Curses.setpos(current_position[:y] + 1, current_position[:x])
   when 'k'
-    Curses.setpos(Curses.stdscr.cury - 1, Curses.stdscr.curx)
+    Curses.setpos(current_position[:y] - 1, current_position[:x])
   when 'l'
-    Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx + 1)
+    Curses.setpos(current_position[:y], current_position[:x] + 1)
   end
 
   if Curses.inch != 32
-  case key
-    when 'h'
-      Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx + 1)
-    when 'j'
-      Curses.setpos(Curses.stdscr.cury - 1, Curses.stdscr.curx)
-    when 'k'
-      Curses.setpos(Curses.stdscr.cury + 1, Curses.stdscr.curx)
-    when 'l'
-      Curses.setpos(Curses.stdscr.cury, Curses.stdscr.curx - 1)
-    end
+    Curses.setpos(current_position[:y], current_position[:x])
   end
 end
 
